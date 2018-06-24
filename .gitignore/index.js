426dbb59d7c336024f84fc3c009c41bbe7caa3b3
Dqms <40544467+Dqms@users.noreply.github.com>
2018-06-24 20:41:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,18 +15,8 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes \n -*help");
     }
+ 
     
-    if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-    }
-        
-
     if (message.content === "Salut"){
         message.reply("Bonjour.");
         console.log("Commande Salut effectuée");
