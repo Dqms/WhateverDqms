8fae6d8cea83ef95a829c3457ce1ee8d8d5e2c55
Dqms <40544467+Dqms@users.noreply.github.com>
2018-06-24 18:58:23 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: *help")
+    console.log("Connected");
+});
+
+bot.login("NDYwNDU2Mzc3NDA0NDg5NzM4.DhFRMw.01K1YaKD6Y2o7Yr2e3n2vMu_arU");
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes \n -*help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour.");
+        console.log("Commande Salut effectuée");
+    }
+});
+
+     
+
+
+
