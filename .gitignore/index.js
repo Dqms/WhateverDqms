b2eb5c4fac0b9420568094cb19c7b6ee6647c50b
Dqms <40544467+Dqms@users.noreply.github.com>
2018-06-26 21:42:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,7 +18,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
             .setColor('#005DFD')
-            .addField("WhatEver Commands list :", "(use the prefix : *) \n \n **Dqms** - *Get the Dqms's stats (#spambotfortnite)* \n **Youtube** - *Get the Dqms's Youtube channel* \n **Info** - *Get the bot infos*")
+            .addField("WhatEver Commands list :", "(use the prefix : *) \n \n **Dqms** - *Get the Dqms's stats (#spambotfortnite)* \n **Youtube** - *Get the Dqms's Youtube channel* \n **Info** - *Get the bot infos* \n _Invite_ - **Get the link to invite someone**")
         message.channel.sendEmbed(help_embed);
     }
 
@@ -34,8 +34,8 @@ bot.on('message', message => {
         message.channel.sendMessage("WhatEver Bot Infos : \n \n **Code** : Node \n **Developed by** : Dqms#5186  \n **Version** : Beta");
     }
 
-    if (message.content === prefix + "idk"){
-        message.channel.sendMessage("WhatEver Bot Infos : \n \n **Code** : Node \n **Developed by** : Dqms#5186  \n **Version** : Beta");
+    if (message.content === prefix + "invite"){
+        message.channel.sendMessage("If you want to invite someone : https://discord.gg/kQ6aV3A");
     }
 
     if (message.content === "Salut"){
