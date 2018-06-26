4181361798ad9e94862158659ef5f581baf07472
Dqms <40544467+Dqms@users.noreply.github.com>
2018-06-26 21:47:55 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,7 +18,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
             .setColor('#005DFD')
-            .addField("WhatEver Commands list :", "(use the prefix : *) \n \n _*Dqms - **Get the Dqms's stats (#spambotfortnite)** \n _*Youtube_ - **Get the Dqms's Youtube channel** \n _*Info_ - **Get the bot infos** \n _*Invite_ - **Get the link to invite someone**")
+            .addField("WhatEver Commands list :", " \n *Dqms - **Get the Dqms's stats (#spambotfortnite)** \n *Youtube - **Get the Dqms's Youtube channel** \n *Twitch - **Get the Dqms's Twitch channel** \n *Info - **Get the bot infos** \n *Invite - **Get the link to invite someone**")
         message.channel.sendEmbed(help_embed);
     }
 
@@ -36,6 +36,10 @@ bot.on('message', message => {
 
     if (message.content === prefix + "invite"){
         message.channel.sendMessage("If you want to invite someone : https://discord.gg/kQ6aV3A");
+    }
+
+    if (message.content === prefix + "twitch"){
+        message.channel.sendMessage("Dqms's Twitch channel : https://www.twitch.tv/therealdqms ");
     }
 
     if (message.content === "Salut"){
