ec874c7ea9e1ddb86b8bce214907915c4e7357da
Dqms <40544467+Dqms@users.noreply.github.com>
2018-10-30 01:07:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,7 +17,7 @@ bot.on('guildMemberAdd', member => {
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
-            .setColor('#8678b5')
+            .setColor('#57b1e9')
             .addField("WhatEver Commands list :", " \n  *Dqms - **Voir les stats de Dqms (Uniquement dans le channel #commands-fortnite)** \n*Youtube - **Voir la chaîne Youtube de Dqms** \n*Twitch - **Voir la chaîne Twitch de Dqms** \n*Invite - **Avoir le lien d'invitation officiel** \n*Info - **Informations du bot**")
         message.channel.sendEmbed(help_embed);
     }
@@ -28,28 +28,28 @@ bot.on('message', message => {
     
     if (message.content === prefix + "youtube"){
         var info_embed = new Discord.RichEmbed()
-            .setColor('#8678b5')
+            .setColor('#57b1e9')
             .addField("WhatEver Youtube :", " \n  Chaîne Youtube de Dqms : <https://bit.ly/2ttEetU>")
         message.channel.sendEmbed(info_embed);
     }
     
     if (message.content === prefix + "info"){
         var info_embed = new Discord.RichEmbed()
-            .setColor('#8678b5')
+            .setColor('#57b1e9')
             .addField("WhatEver Bot Info :", " \n  Code : **Node** \nDéveloppé : **Dqms#5740**  \nVersion : **1.1**")
         message.channel.sendEmbed(info_embed);
     }
 
     if (message.content === prefix + "invite"){
         var info_embed = new Discord.RichEmbed()
-            .setColor('#8678b5')
+            .setColor('#57b1e9')
             .addField("WhatEver Invite :", "\n Lien d'invitation officiel : https://discord.gg/QqCYD9W")
         message.channel.sendEmbed(info_embed);
     }
 
     if (message.content === prefix + "twitch"){
         var info_embed = new Discord.RichEmbed()
-            .setColor('#8678b5')
+            .setColor('#57b1e9')
             .addField("WhatEver Twitch :", "\n Chaîne Twitch de Dqms : https://www.twitch.tv/dqmsofficial ")
         message.channel.sendEmbed(info_embed);
     }
